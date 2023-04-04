00993cdd7c167d1a4205d2c7e78705b93e8091f9
zafran hussain <zaf_hus@yahoo.co.uk>
2023-04-04 23:38:22 +0100

    commit

---
+++ Chapter 1 PY4E.py
@@ -0,0 +1,18 @@
+x = 11
+if x < 10:
+    print ("smaller")
+if x > 20:
+    print ("bigger")
+print ("finish")
+
+# below config is exaple of a loop which has iteration variables that change each time through a loop until it get to 0 
+# then it will print blastoff
+n = 10
+while n > 0 :
+    print (n)
+    n = n + - 1
+print ("blastoff")
+
+
+ 
+
---
+++ Chapter 2 PY4e.py
@@ -0,0 +1,46 @@
+# Variables, expression and statements
+
+x = 12.2
+y = 14
+
+x = 12.2
+y = 14
+x = 100
+
+# the below config is confusing and inconveniant
+vijhbvikjhbf = 35.0
+vijhbvikjhbflmon = 12.50
+vijhbvikjhbf = vijhbvikjhbf * vijhbvikjhbflmon
+print (vijhbvikjhbf)
+
+#The below config has the same value as the above however is more simpler 
+a = 35.0
+b = 12.50
+c = a * b
+print (c)
+
+#the below is the most simplest understanding of the what we are tring to achive
+hours = 35.0
+rate = 12.50
+pay = hours * rate
+print (pay)
+
+#expressions
+#the below eee variable is a example of concatenanate i.E put together hello + there = hello there
+eee = "hello" + " there"
+print (eee)
+
+#if you look at the first two print statments these are using intergers hower the print output is a floating number 
+# (I.E it is a decimal number)
+print (10 /2)
+print (9 / 2 )
+print ( 99 / 100)
+print (10.0 / 2.0)
+print (99.0 / 100.0)
+
+
+#the below uses a user INPUT which will then continue to story
+nam : input('who are you ? ')
+print('welcome' , nam)
+
+
