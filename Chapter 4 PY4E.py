c3e78b7685b76c527b23d70909fb1d53ae910a3f
zafran hussain <zaf_hus@yahoo.co.uk>
2023-04-05 22:08:40 +0100

    completed chapter 4

--- Chapter 4 PY4E.py
+++ Chapter 4 PY4E.py
@@ -1,7 +1,26 @@
-def thing ()
+
+#example of stored and reused steps I.E creating your own function with the def.
+# the def creates a function but does not run the code in the first parahg untill you call or invoke it 
+def thing ():
     print ("hello")
     print ("fun")
 
+#below show the thing function which was created above being referenced 
 thing ()
 print("zip")
 thing ()
+#as thing is a reusable peice of code we have created, we call this a function/s
+
+
+
+########  Building our own function  ################
+
+#We create functionsusing the def keywoard follow by optional paramaters in parentheses
+#we indent the body of the function
+#this defines the funtion but does NOT EXECUTE the body of the function
+
+def print_lyrics ():
+    print ("I want it that way")
+    print ("tell me why")
+    print ("aint nothing but a heartache tell me why")
+
