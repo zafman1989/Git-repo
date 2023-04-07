1aafa2d96c28d2d653ed18d23cac82d81aeedc61
zafran hussain <zaf_hus@yahoo.co.uk>
2023-04-07 16:58:35 +0100

    example of input function

---
+++ ipvzeo/Input Funtction.py
@@ -0,0 +1,7 @@
+
+############input funtion#############
+
+"""the below command will ask for user input in the terminal with the prompt text of "enter the show command you wish to execute: " """
+show_command = input (" enter the show command you wish to execute: ") 
+print (show_command)
+
